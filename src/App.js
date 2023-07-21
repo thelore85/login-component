b3dfd8ab10aa0db4425e47eb2229307b33c7deb9
TheLore85 <garofalo.digital@gmail.com>
2023-07-21 12:41:05 +0200

    debugging index.html: added <script>process=test;

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import Menu from './components/Menu/Menu.js';
 import Main from './components/Main/Main.js';
 import Footer from './components/Footer/Footer.js';
 import Background from './components/Background/Background.js'
+import Clarifai from 'clarifai';
 
 // css
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+
+// //API clraify: configuration
+const app = new Clarifai.App({
+	apiKey: '87a0584c90e64c87869205181c5b18a7',
+});
+
 class App extends Component{
+
+onSearchClick = () => {
+	console.log('click', app)
+}
+
+onInputChange = (event) => {
+	console.log('input change:', event.target.value);
+}
+
 	render(){
 		return(
 			<div className='app-container'>
 				<Menu />
 				<Background />
-				<Main />
+				<Main onSearchClick={this.onSearchClick} onInputChange={this.onInputChange}/>
 				<Footer />
 			</div>
 		)
 	}
+
 }
 
 export default App
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Main.css';
 
-const Main = () => {
+const Main = ({ onSearchClick, onInputChange }) => {
 	return(
 		<section id="main">
 				<h1>Face identification</h1>
 				<div className="wrapper">
 					<h2>Faces detecred on image: #5</h2>
-					<input className="input" type="search"></input>
-					<button type="button" className="btn btn-primary"><i className="fas fa-search"></i></button>           
+					<input className="input" type="search" onChange={onInputChange}></input>
+					<button type="button" className="btn btn-primary" onClick={onSearchClick}><i className="fas fa-search"></i></button>           
 				</div>
 		</section>
 	)
