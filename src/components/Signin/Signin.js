24fabcfadc7f69ee4a8f3c7d56a2b8e5b72c7ca9
TheLore85 <garofalo.digital@gmail.com>
2023-08-03 08:16:59 +0200

    feature: sign in integration with server

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,58 +1,107 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import './Signin.css';
 
-const Signin = ({ onRouteChange }) => {
+class Signin extends Component {
 
-  return(
-    <div id="signin">
+  constructor(props){
+    super(props);
+    this.state = {
+      signInEmail : '',
+      signInPsw : '',
+    }
+  }
+  
+  onEmailChange = (event) => {
+    this.setState({
+      signInEmail : event.target.value,
+    })
+  }
 
-      <div className="container h-100">
-        <div className="d-flex justify-content-center h-100">
-          <div className="user_card">
+  onPswChange = (event) => {
+    this.setState({
+      signInPsw : event.target.value,
+    })
+  }
 
-            <div className="d-flex justify-content-center">
-              <div className="brand_logo_container">
-                <i className=" brand_logo fa-solid fa-robot"></i>
+  onSubmitSignin = () => {
+    // console.log( this.state.signInEmail)
+    // console.log( this.state.signInPsw)
+
+    fetch('http://localhost:9000/signin',
+    {
+      method: 'post',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({
+        email: this.state.signInEmail,
+        password: this.state.signInPsw
+       })
+      })
+    .then(response => response.json())
+    .then(data => {
+      if(data === 'success'){this.props.onRouteChange('home')}
+      else{console.log('error log in')}
+    })
+  }
+  
+
+  render(){
+
+    return(
+      <div id="signin">
+  
+        <div className="container h-100">
+          <div className="d-flex justify-content-center h-100">
+            <div className="user_card">
+  
+              <div className="d-flex justify-content-center">
+                <div className="brand_logo_container">
+                  <i className=" brand_logo fa-solid fa-robot"></i>
+                </div>
               </div>
-            </div>
-
-            <div className="d-flex justify-content-center form_container">
-              <form>
-
-                <div className="input-group mb-3">
-                  <div className="input-group-append">
-                    <span className="input-group-text"><i className="fa-solid fa-envelope"></i></span>
+  
+              <div className="d-flex justify-content-center form_container">
+                <form>
+  
+                  <div className="input-group mb-3">
+                    <div className="input-group-append">
+                      <span className="input-group-text"><i className="fa-solid fa-envelope"></i></span>
+                    </div>
+                    <input id="signInUser" type="text" name="" className="form-control input_user" placeholder="email" onChange={this.onEmailChange}/>
                   </div>
-                  <input type="text" name="" className="form-control input_user" placeholder="email"/>
-                </div>
-
-                <div className="input-group mb-2">
-                  <div className="input-group-append">
-                    <span className="input-group-text"><i className="fas fa-key"></i></span>
+  
+                  <div className="input-group mb-2">
+                    <div className="input-group-append">
+                      <span className="input-group-text"><i className="fas fa-key"></i></span>
+                    </div>
+                    <input id="signiInPassword" type="password" name="" className="form-control input_pass" placeholder="password" onChange={this.onPswChange}/>
                   </div>
-                  <input type="password" name="" className="form-control input_pass" placeholder="password" />
-                </div>
-
-                <div className="d-flex justify-content-center mt-3 login_container">
-                    <button type="button" name="button" className="btn login_btn" onClick={() => onRouteChange('home')}>Login</button>
-                </div>
-
-              </form>
-            </div>
-        
-            <div className="mt-4">
-              <div className="d-flex justify-content-center links">
-                <span className="plain-text">No Account?</span>
-                <a href="#" onClick={() => onRouteChange('register')} className="ml-2">Register Now</a>
+  
+                  <div className="d-flex justify-content-center mt-3 login_container">
+                      <button type="button" name="button" className="btn login_btn" onClick={this.onSubmitSignin}>Login</button>
+                  </div>
+  
+                </form>
               </div>
-            </div>
+          
+              <div className="mt-4">
 
+                <div className="d-flex justify-content-center links">
+                  <span className="plain-text">No Account?</span>
+                  <a href="#" onClick={() => this.props.onRouteChange('register')} className="ml-2">Register Now</a>
+                </div>
+
+              </div>
+  
+            </div>
           </div>
         </div>
+  
       </div>
+    );
 
-    </div>
-  );
+
+  }
 }
 
 export default Signin;
