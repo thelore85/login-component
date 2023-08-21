a3e485d863e3b02886a8053de9565043eaec6751
TheLore85 <garofalo.digital@gmail.com>
2023-08-21 10:27:49 +0200

    adding server folder

---
+++ client/src/App.js
@@ -0,0 +1,237 @@
+import React, { Component } from 'react';
+import Menu from './components/Menu/Menu.js';
+import Main from './components/Main/Main.js';
+import Footer from './components/Footer/Footer.js';
+import Background from './components/Background/Background.js'
+import Signin from './components/Signin/Signin.js';
+import Register from './components/Register/Register.js';
+
+// css
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+//global var
+let input = '';
+
+// CLRAIFAI API NEW 
+const PAT = '87a0584c90e64c87869205181c5b18a7';
+const USER_ID = 'thelore_85';       
+const APP_ID = 'biometrics';
+const MODEL_ID = 'face-detection';
+let IMAGE_URL = '';
+ 
+///////////////////////////////////////////////
+////////---------COMPONENT ------------////////
+///////////////////////////////////////////////
+
+
+/////////////////////////////////////
+// Constructor
+
+const initialState = {
+      url: '',
+      boxCoordinates: {},
+      route: 'signin',
+      isSignIn: false,
+      user: {
+          id: '',
+          name: '',
+          email: '',
+          joined: '',
+        },
+      session: {
+        email: '',
+        entries: 0,
+        sessions: 1,
+        last_login: 1,
+        img_search: [],
+        sessions: 0,
+      }
+    }
+
+class App extends Component{
+  
+  //set the constructor
+  constructor(){
+    super();
+    this.state = initialState;
+  }
+
+  // API call: Image recognition
+  onSearchClick = () => {
+
+    //update state with image info
+    this.setState({
+      url: IMAGE_URL,
+    })
+  
+    // set clarifai api variables
+    const raw = JSON.stringify({
+      "user_app_id": { "user_id": USER_ID, "app_id": APP_ID},
+      "inputs": [{ "data": { "image": {"url": IMAGE_URL}}}]
+    });
+
+    const requestOptions = {  
+      method: 'POST', 
+      headers: { 'Accept': 'application/json','Authorization': 'Key ' + PAT },
+      body: raw
+    };
+
+    //run api prediction
+     fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+      .then(response => {
+        if(response.ok){
+          this.uploadSession();
+          return response.text()
+        }
+      })
+      .then(result => this.faceDetection((this.calculateFaceBox(JSON.parse(result)))))// returning the prediction and the sqaure details
+      .catch(error => console.log('ERROR: CLARIFY API')); 
+
+  }
+
+  //reset state after new user login
+  resetState = () => {
+    this.setState( initialState )
+  }
+
+  ////////////////////////////////
+  // set state with face box coordinates
+  faceDetection = (box) => { 
+    this.setState({
+      boxCoordinates: box,
+    })
+  }
+
+  // calculate face box coordinates
+  calculateFaceBox = (predictionObj) => { 
+    const coordinates = predictionObj.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('input-image');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    const left = coordinates.left_col * width;
+    const right = coordinates.right_col * width;
+    const top = coordinates.top_row * height;
+    const bottom = coordinates.bottom_row * height;
+  
+    //return 'box' for 'faceDEtection' function;
+    return { 
+      left: coordinates.left_col * (width),
+      right: width - (coordinates.right_col * width),
+      top: (coordinates.top_row * height),
+      bottom: height - (coordinates.bottom_row * height),
+      
+      // calculate box dimention
+      width: right-left,       // in %, right-left  = (60%-30%) = 30% 
+      height: bottom-top,      // in %, bottom-top  = (80%-60%) = 20%
+    }
+  }
+
+  onInputChange = (event) => {
+    IMAGE_URL = event.target.value;
+  };
+
+  //ROUTING management
+  onRouteChange = (newRoute) => {
+
+    //SET newRoute parameter after click interaction
+    this.setState({ 
+      route: newRoute
+    })
+
+    // assign isSignIn state based on newRoute parameter (click interaction)
+    if(newRoute === 'signin' || newRoute === 'register'){
+      this.setState({
+        isSignIn: false
+      })
+    }else{
+      this.setState({
+        isSignIn: true
+      })
+    }
+  }
+
+  //get data from register component and push to state
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        joined: data.data_creation,
+        sessions: data.sessions
+      }  
+    });
+  }
+
+  // UPDATE session state with url search AND update DB with new session data
+  uploadSession = () => {
+  
+    // delate last img in the array
+    if(this.state.session.img_search.length >=10){
+      this.state.session.img_search.pop();
+    }
+  
+    // add img on top of the img_search array
+    this.state.session.img_search.unshift(IMAGE_URL); 
+  
+    //update entries value
+    let newEntries = Number(this.state.session.entries) + 1
+
+  
+    //send the new session state to the db
+    fetch('http://localhost:9000/session-update',
+      {
+        method: 'put',
+        headers: {'Content-Type' : 'application/json'},
+        body: JSON.stringify({
+          email: this.state.user.email,
+          last_login: new Date(),
+          img_search: this.state.session.img_search,
+          entries: newEntries
+        })
+      })
+      .then(response => response.json())
+      .then(session => this.loadSession(session))
+    }
+
+  //get data from server: session-update / session-post and update App.js state
+  loadSession = (data) => {
+    this.setState({
+      session: {
+        email: data.email,
+        entries: data.entries,
+        last_login: new Date(data.last_login).toLocaleDateString('it-IT'),
+        img_search: data.img_search
+      }
+    })
+  }
+
+  // on load page
+  componentDidMount(){  
+  }
+
+  // RENDER THE COMPONENT
+	render(){
+    // console.log( 'user:', this.state.user)
+    // console.log( 'session:', this.state.session)
+		return(
+      
+			<div className='app-container'>
+				<Menu onRouteChange={this.onRouteChange} isSignIn={this.state.isSignIn} />
+				<Background />
+        { this.state.route === 'home'
+          ? <Main user={this.state.user} session={this.state.session} onSearchClick={this.onSearchClick} onInputChange={this.onInputChange} url={this.state.url} box={this.state.boxCoordinates}/>
+          : (
+            this.state.route === 'signin'
+            ?<Signin loadUser={this.loadUser} loadSession={this.loadSession} onRouteChange={this.onRouteChange} resetState={this.resetState}/>
+            :<Register loadUser={this.loadUser} loadSession={this.loadSession} onRouteChange={this.onRouteChange} resetState={this.resetState}/>
+          )
+        }
+				  <Footer />
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,163 @@
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+/////// ------ NODE.JS SERVER ------- ////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+
+//////////////////////////////////////
+// SERVER SETTING
+
+//set-up the server
+const express = require('express'); // server framework
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');  // db framework
+
+// activate plug-ins
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// server launch
+app.listen(9000, ()=>{ console.log('app is running on port: 9000') })
+
+
+///////////////////////////////
+// DATABASE CONNECTION 
+
+// PostgreSql connection
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1', //localhost
+    port : 5432, //default port psql
+    user : '',
+    password : '',
+    database : 'image_recognition' // db name 
+  }
+});
+
+
+
+
+//////////////////////////////////////////////////////
+//////// ---------   END POINTS  --------------------
+//////////////////////////////////////////////////////
+
+
+/////////////////////////////////
+//signin: check user data --> return user to front end
+app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+
+  var hash = bcrypt.hashSync(password);
+
+
+  db.select('*')
+    .from('users')
+    .where({ email })
+    .andWhere(
+      bcrypt.compareSync( password , hash)
+    )
+    .then(user => {
+      if (user.length > 0) {
+        res.json(user[0]); // Restituisci il primo utente trovato
+      } else {
+        res.json({}); // res empty obj: to preserve front-end error (if undefined the fetch in signin compo. run error)
+      }
+    })
+    .catch(err => res.status(400).json('ERROR: server /signin'));
+});
+
+
+/////////////////////////////////
+//register: add a user record -> return new user object
+app.post('/register', (req,res) => {
+  
+  const { name, email, password } = req.body;
+
+  var hash = bcrypt.hashSync( password );
+
+  db('users')
+  .returning('*') //this method give a response from db
+  .insert({
+    email: email,
+    name: name,
+    password: hash,
+    data_creation: new Date().toLocaleString(),
+  })
+  .then(user => res.json(user[0]))
+  .catch(err => res.status(400).json('impossibile to register:', err))
+
+})
+
+
+/////////////////////////////////////
+//Session Post: create a new sessin record (only during registration) --> return session
+app.post('/session-post', (req,res) =>{
+
+  //retrive user input from fornt end
+  const { email, img_search, entries, sessions } = req.body;
+
+  // create a new login record for new user 
+  db('login')
+  .returning('*')
+  .insert({
+    email: email,
+    last_login: new Date(),
+    img_search: img_search,
+    entries: entries,
+    sessions: sessions
+  })
+  .then(session => 
+    {
+      return res.json(session[0])
+    }
+  )
+  .catch(err => res.status(400).json('impossibile to register:', err))
+
+})
+
+
+/////////////////////////////////////////// 
+// session load: load session after login -> send back session information to front-end
+app.put('/session-load', (req, res) =>{
+
+  //retrive user input from fornt end
+  const { email } = req.body;
+
+  // check db if user exist -> return user
+  db.select('*')
+    .from('login')
+    .where({ email })
+    .increment('sessions', 1)
+    .returning('*')
+    .then( session => {
+      if(session.length > 0){ 
+        res.json(session[0])}
+      else{ res.json({}) }
+    })
+    .catch(err => res.status(400).json('ERROR: server /session-load no user match'))
+})
+
+
+//////////////////////////////////// 
+//session update: update login DB after img detection (search click)
+app.put('/session-update', (req, res) => {
+  const { email, last_login, img_search, entries } = req.body;
+  
+  db('login')
+  .where({ email })
+  .update({
+    img_search: img_search,
+    last_login: last_login,
+    entries: entries
+  })
+  .returning('*')
+  .then(session => res.json(session[0])) // we don't really need it, we can use state in front end
+  .catch(err => res.status(400).json('ERROR: session-update /server'))  
+
+  });
+
