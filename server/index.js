e79283b9f6d18e7e5a972c912c2c1c42d5a49b92
TheLore85 <garofalo.digital@gmail.com>
2023-08-23 13:59:20 +0200

    deploy server: all routing v1

--- server/index.js
+++ server/index.js
@@ -70,9 +70,7 @@ const db = knex({
 
 
 /////////////////////////////////
-//signin: check user data --> return user to front end
-
-
+//main root: check user data --> return user to front end
 app.get('/', (req, res) =>{
   // res.status(200).json(`server is up and running - live port: ${port};`)
   
@@ -105,3 +103,97 @@ app.post('/signin', (req, res) => {
     })
     .catch(err => res.status(400).json('ERROR: server /signin'));
 });
+
+
+/////////////////////////////////
+//register: add a user record -> return new user object
+app.post('/register', (req,res) => {
+  
+  const { name, email, password } = req.body;
+
+  var hash = bcrypt.hashSync( password );
+
+  db('users')
+  .returning('*') //this method give a response from db
+  .insert({
+    email: email,
+    name: name,
+    password: hash,
+    data_creation: new Date().toLocaleString(),
+  })
+  .then(user => res.json(user[0]))
+  .catch(err => res.status(400).json({ message: 'ERROR: /register - impossibile to register', error: err }))
+
+})
+
+
+
+/////////////////////////////////////
+//Session Post: create a new sessin record (only during registration) --> return session
+app.post('/session-post', (req,res) =>{
+
+  //retrive user input from fornt end
+  const { email, img_search, entries, sessions } = req.body;
+
+  // create a new login record for new user 
+  db('sessions')
+  .returning('*')
+  .insert({
+    email: email,
+    last_login: new Date(),
+    img_search: img_search,
+    entries: entries,
+    sessions: sessions
+  })
+  .then(session => 
+    {
+      return res.json(session[0])
+    }
+  )
+  .catch(err => res.status(400).json({ message: 'ERROR: session-post', error: err }))
+
+})
+
+
+/////////////////////////////////////////// 
+// session load: load session after login -> send back session information to front-end
+app.put('/session-load', (req, res) =>{
+
+  //retrive user input from fornt end
+  const { email } = req.body;
+
+  // check db if user exist -> return user
+  db.select('*')
+    .from('sessions')
+    .where({ email })
+    .increment('sessions', 1)
+    .returning('*')
+    .then( session => {
+      if(session.length > 0){ 
+        res.json(session[0])}
+      else{ res.json({}) }
+    })
+    .catch(err => res.status(400).json({ message: 'ERROR: session-load', error: err }))
+})
+
+
+//////////////////////////////////// 
+//session update: update login DB after img detection (search click)
+app.put('/session-update', (req, res) => {
+  const { email, last_login, img_search, entries } = req.body;
+
+  console.log('DEBUGGING: server - session-update ', img_search)
+  
+  db('sessions')
+  .where({ email })
+  .update({
+    img_search: img_search,
+    last_login: last_login,
+    entries: entries
+  })
+  .returning('*')
+  .then(session => res.json(session[0])) // we don't really need it, we can use state in front end
+  .catch(err => res.status(400).json({ message: 'ERROR: session-update', error: err }))  
+
+  });
+
