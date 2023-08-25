f9ce95a135bd906f2d42f4b96bf821c81fb8a071
TheLore85 <garofalo.digital@gmail.com>
2023-08-25 11:49:25 +0200

    server deploy: vercel.json v4

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ let input = '';
 
 export let serverUrl;
 const serverPort = 9000;
-const serverPath = '/api'
+const serverPath = '/site'
 const serverAdress = 'https://login-component-server.vercel.app';
 const serverLive = `${serverAdress}${serverPath}`;
 const serverLocal = `http://localhost:${serverPort}`
